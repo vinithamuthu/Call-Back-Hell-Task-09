6c8c3e3b2e3cfa4ad4e77edc9e9970570be0af0b
vinithamuthu <vinisurya.m@gmail.com>
2023-12-01 23:39:31 +0530

    task completed

---
+++ script.js
@@ -0,0 +1,67 @@
+
+
+
+setTimeout(()=>{
+
+  document.getElementById("timer").innerHTML=10;
+
+  setTimeout(()=>{
+
+    document.getElementById("timer").innerHTML=9;
+
+    setTimeout(()=>{
+
+      document.getElementById("timer").innerHTML=8;
+
+      setTimeout(()=>{
+
+        document.getElementById("timer").innerHTML=7;
+
+        setTimeout(()=>{
+
+          document.getElementById("timer").innerHTML=6;
+
+          setTimeout(()=>{
+
+            document.getElementById("timer").innerHTML=5;
+
+            setTimeout(()=>{
+
+              document.getElementById("timer").innerHTML=4;
+
+              setTimeout(()=>{
+
+                document.getElementById("timer").innerHTML=3;
+
+                setTimeout(()=>{
+
+                  document.getElementById("timer").innerHTML=2;
+
+                  setTimeout(()=>{
+
+                    document.getElementById("timer").innerHTML=1;
+
+                    setTimeout(()=>{
+
+                      document.getElementById("timer").innerHTML="<span class=cls>Happy Independence Day</span>";
+                    
+                    },1000);
+                  
+                  },1000);
+                
+                },1000);
+              
+              },1000);
+            
+            },1000);
+          
+          },1000);
+        
+        },1000);
+      
+      },1000);
+    
+    },1000);
+  
+  },1000);
+},1000);
